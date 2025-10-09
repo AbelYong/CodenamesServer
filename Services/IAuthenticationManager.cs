0d438c57c924d9c6bcae6cc57338cff63d419d63
AbelYong <zs23014089@estudiantes.uv.mx>
2025-10-08 22:52:14 -0600

    Project restructuring

---
+++ Services/IAuthenticationManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Services
+{
+    [ServiceContract]
+    public interface IAuthenticationManager
+    {
+        [OperationContract]
+        bool Login(User user);
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+    [DataContract]
+    public class User
+    {
+        private System.Guid userID;
+        private string email;
+        private string password;
+
+        [DataMember]
+        public System.Guid UserID {  get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
