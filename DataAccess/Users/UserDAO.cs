c2f83c1447a98b9e7a8dd5317fabecb393a823dc
AbelYong <zs23014089@estudiantes.uv.mx>
2025-10-08 23:07:00 -0600

    Data access project added

---
+++ DataAccess/Users/UserDAO.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class UserDAO : IUserDAO
+    {
+        public bool Login(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Guid GetUserFromEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
