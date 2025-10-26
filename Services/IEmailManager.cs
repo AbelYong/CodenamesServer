6c54c5dad79b753d32dd7b66584d629b60217295
AbelYong <zs23014089@estudiantes.uv.mx>
2025-10-26 12:25:11 -0600

    Removed unused imports in service contracts

--- Services/IEmailManager.cs
+++ Services/IEmailManager.cs
@@ -1,10 +1,5 @@
 ﻿using Services.DTO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
 
 namespace Services
 {
