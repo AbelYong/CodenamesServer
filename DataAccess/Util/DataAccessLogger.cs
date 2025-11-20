e934d4a22674b441eab5f279c90699744537dea5
AbelYong <zs23014089@estudiantes.uv.mx>
2025-11-20 00:00:36 -0600

    Added logger to DataAccess

---
+++ DataAccess/Util/DataAccessLogger.cs
@@ -0,0 +1,9 @@
+﻿using log4net;
+
+namespace DataAccess.Util
+{
+    public static class DataAccessLogger
+    {
+        public static readonly ILog Log = LogManager.GetLogger(typeof(DataAccessLogger));
+    }
+}
