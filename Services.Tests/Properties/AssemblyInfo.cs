67789e49c4529d2dbc8bcedcc23fd51196c22f35
AbelYong <zs23014089@estudiantes.uv.mx>
2025-11-20 23:49:38 -0600

    Added service test project

---
+++ Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Services.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a846976c-ac8a-454b-9cf7-2ed9e8b205c6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Services.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Services.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            int a = 1;
+            int b = 2;
+            int sum = a + b;
+            Assert.That(sum, Is.EqualTo(a + b));
+        }
+    }
+}
