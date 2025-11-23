ce1eb7c32ece73aa6849bebfad2e89423647f4fc
AbelYong <zs23014089@estudiantes.uv.mx>
2025-11-22 23:49:04 -0600

    Migration of DataAccess.Test a DataAccess.Tests

--- DataAccess.Tests/ProfileTest.cs
+++ DataAccess.Tests/ProfileTest.cs
@@ -4,10 +4,6 @@ using DataAccess.Util;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess.Test
 {
