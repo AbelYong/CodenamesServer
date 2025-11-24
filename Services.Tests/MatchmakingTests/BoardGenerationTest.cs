a17b59de542309a0c5227213bce4786b7a449078
AbelYong <zs23014089@estudiantes.uv.mx>
2025-11-24 12:41:31 -0600

    Updated Services.Tests

--- Services.Tests/MatchmakingTests/BoardGenerationTest.cs
+++ Services.Tests/MatchmakingTests/BoardGenerationTest.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Services.Operations;
+using Services.DTO.DataContract;
 
 namespace Services.Tests.MatchmakingTests
 {
@@ -12,6 +13,12 @@ namespace Services.Tests.MatchmakingTests
     public class BoardGenerationTest
     {
         [Test]
-
+        public void GenerateMatch_Requester_MatchesConfiguration()
+        {
+            MatchConfiguration config = new MatchConfiguration();
+            config.Requester = new DTO.Player();
+            config.Requester.PlayerID = Guid.NewGuid();
+            Guid requesterID = (Guid)config.Requester.PlayerID;
+        }
     }
 }
